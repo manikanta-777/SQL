6475f7271885708f05792eaab27941ade1f1c3d8
Manikantha Chintakayala <chmanikanta777@gmail.com>
2025-11-04 02:54:18 +0530

    Create joins Challeges.sql
    Added joins challenge solutions

---
+++ joins Challeges.sql
@@ -0,0 +1,33 @@
+-- JOINS challenges
+
+-- challenge 1 
+SELECT * FROM customer;
+SELECT * FROM address;
+
+SELECT email,address.address_id, district from customer
+JOIN address
+ON address.address_id = customer.address_id
+WHERE address.district = 'California';
+
+SELECT email, district from customer
+JOIN address
+ON address.address_id = customer.address_id
+WHERE address.district = 'California';
+
+--challenge 2
+SELECT * FROM actor
+WHERE first_name = 'Nick' AND last_name = 'Wahlberg';
+
+SELECT * FROM film_actor
+WHERE actor_id = 2;
+
+
+SELECT film_actor.actor_id, first_name, last_name, film_actor.film_id, film.title from actor
+LEFT JOIN film_actor
+ON film_actor.actor_id = actor.actor_id
+LEFT JOIN film
+ON film_actor.film_id = film.film_id
+WHERE first_name = 'Nick' AND last_name = 'Wahlberg'
+
+
+
